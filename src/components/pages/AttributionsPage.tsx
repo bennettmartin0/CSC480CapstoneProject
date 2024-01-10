8e7c3ddec3cd3f4a186a4d80d838ac3a6a25957c
Bennett Martin <bennett.martin@csuglobal.edu>
2024-01-10 11:17:01 -0500

    update image

--- src/components/pages/AttributionsPage.tsx
+++ src/components/pages/AttributionsPage.tsx
@@ -1,45 +1,45 @@
 import { PageTemplate } from "./PageTemplate";
 
 export const AttributionsPage = () => (
-    <PageTemplate header="Attributions" headerImageURI="./src/assets/General4.jpg" showGauge={false}>
+    <PageTemplate header="Attributions" headerImageURI="https://upload.wikimedia.org/wikipedia/commons/e/e0/Spotting_a_Sea_Turtle.jpg" showGauge={false}>
         <>
         <ul className="list-none space-y-2">
-            <li><a href="https://www.flaticon.com/free-icons/sea-turtle" title="sea turtle icons">Sea Turtle icon created by wanicon - Flaticon</a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Green_Turtle_Chelonia_Mydas_(224075535).jpeg">Green Turtle Chelonia Mydas by Kris Mikael Krister, licensed under CC Attribution 3.0 Unported License</a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Baby_Chelonia_mydas_20060319.jpg">
+            <li><a className="underline" href="https://www.flaticon.com/free-icons/sea-turtle" title="sea turtle icons">Sea Turtle icon created by wanicon - Flaticon</a></li>
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Green_Turtle_Chelonia_Mydas_(224075535).jpeg">Green Turtle Chelonia Mydas by Kris Mikael Krister, licensed under CC Attribution 3.0 Unported License</a></li>
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Baby_Chelonia_mydas_20060319.jpg">
             Green Turtle (Chelonia mydas) just hatched by Manuel Heinrich, licensed under CC Attribution-Share Alike 2.5 Generic License
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Spotting_a_Sea_Turtle.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Spotting_a_Sea_Turtle.jpg">
             A sea turtle swimming through the ocean in Maui, Hawaii. by Ashley Lee, licensed under CC Attribution 4.0 International License
             </a></li>
             <li>
-                <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Chelonia_Mydas_%28cropped%29.JPG/1200px-Chelonia_Mydas_%28cropped%29.JPG?20180307235014">Chelonia mydas in the Caribbean sea off the coast of Curacao by Bthv, licensed under the CC Attribution-Share Alike 3.0 Unported</a>
+                <a className="underline" href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Chelonia_Mydas_%28cropped%29.JPG/1200px-Chelonia_Mydas_%28cropped%29.JPG?20180307235014">Chelonia mydas in the Caribbean sea off the coast of Curacao by Bthv, licensed under the CC Attribution-Share Alike 3.0 Unported</a>
             </li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Figure_1_-_Map_of_Jekyll_Island.png">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Figure_1_-_Map_of_Jekyll_Island.png">
             Illustration from A Field Guide to Jekyll Island (1983). by Carol Johnson (illustrator), Public Domain.
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:108_Georgia_Sea_Turtle_Center,_Jekll_Island,_Georgia.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:108_Georgia_Sea_Turtle_Center,_Jekll_Island,_Georgia.jpg">
             Georgia, 2010. by XeresNelro, licensed under CC Attribution 4.0 International License
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:21-06-025-beach.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:21-06-025-beach.jpg">
             Driftwood Beach - Jekyll Island, GA. by Dsdugan, licensed under CC CC0 1.0 Universal Public Domain Dedication
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Loggerhead_sea_turtle.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Loggerhead_sea_turtle.jpg">
             Underside of a loggerhead sea turtle as it swims overhead by Upendra Kanda, licensed under CC Attribution 2.0 Generic License
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Leatherback_sea_turtle_Tinglar,_USVI_(5839996547).jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Leatherback_sea_turtle_Tinglar,_USVI_(5839996547).jpg">
             Scientific name: Dermochelys coriacea by Claudia Lombard, licensed under CC Attribution 2.0 Generic License
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Hawksbill_sea_turtle_-_NOAA.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Hawksbill_sea_turtle_-_NOAA.jpg">
             Chelonia mydas in the Caribbean sea off the coast of Curacao by BTHV, licensed under CC Attribution-Share Alike 3.0 Unported License
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Hawksbill_sea_turtle_-_NOAA.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Hawksbill_sea_turtle_-_NOAA.jpg">
             Hawksbill sea turtle by NOAA, Public Domain
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:A_nesting_Kemp%27s_ridley_sea_turtle_returns_to_the_water_after_nesting_at_the_34.5_mile_marker_at_Padre_Island_National_Seashore_on_8_June_2011_(f70ae6c6-bc62-4654-97be-f0060950efb7).jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:A_nesting_Kemp%27s_ridley_sea_turtle_returns_to_the_water_after_nesting_at_the_34.5_mile_marker_at_Padre_Island_National_Seashore_on_8_June_2011_(f70ae6c6-bc62-4654-97be-f0060950efb7).jpg">
             A nesting Kemp's ridley sea turtle returns to the water after nesting at the 34.5 mile marker at Padre Island National Seashore on 8 June 2011 by U.S. National Park Service, Public Domain
             </a></li>
-            <li><a href="https://commons.wikimedia.org/wiki/File:Olive_ridley_sea_turtle.jpg">
+            <li><a className="underline" href="https://commons.wikimedia.org/wiki/File:Olive_ridley_sea_turtle.jpg">
             Olive Ridley female after laying eggs by Brad Flickinger, licensed under the CC Attribution 2.0 Generic License
             </a></li>
         </ul>
